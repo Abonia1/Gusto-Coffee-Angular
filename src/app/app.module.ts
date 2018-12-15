208e1ef610599575060734d0d750e1afff9d139f
Unknown <aboniaa@gmail.com>
2018-12-15 14:10:38 +0100

    added list and coffee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,14 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
          MatToolbarModule, MatCardModule, MatSlideToggleModule } from "@angular/material";
 import 'hammerjs';
+import { ListComponent } from './list/list.component';
+import { CoffeeComponent } from './coffee/coffee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CoffeeComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule,
---
+++ src/app/coffee/coffee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee',
+  templateUrl: './coffee.component.html',
+  styleUrls: ['./coffee.component.css']
+})
+export class CoffeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
