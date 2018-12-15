899d6d98225ec2a038d361b6bfd98a8d929033e9
Abonia1 <aboniaa@gmail.com>
2018-12-15 13:54:12 +0100

    Interface component with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{GeolocationService} from "./geolocation.service"
-import { AppRoutingModule } from './app-routing.module';
+import { GeolocationService } from "./geolocation.service";
 import { AppComponent } from './app.component';
+import { DataService } from "./data.service";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
+         MatToolbarModule, MatCardModule, MatSlideToggleModule } from "@angular/material";
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, BrowserAnimationsModule,
+    MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
+    MatToolbarModule, MatCardModule, MatSlideToggleModule
   ],
   providers: [GeolocationService,DataService],
   bootstrap: [AppComponent]
