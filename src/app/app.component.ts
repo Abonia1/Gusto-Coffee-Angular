c3761a6aa53f184943e69ac2ca4be5bee9e810cd
Unknown <aboniaa@gmail.com>
2019-01-05 15:47:00 +0100

    Installation suggessestion prompt in browser and non ios devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,26 @@ export class AppComponent {
       this.snackBar.open("You can add this PWA to the Home Screen", 
           "", { duration: 3000 });
     }
-    
+    if ((navigator as any).standalone==undefined) {
+      // It's not iOS
+      if (window.matchMedia("(display-mode: browser").matches) {
+        // We are in the browser
+        window.addEventListener("beforeinstallprompt", event => {
+          event.preventDefault();
+          const sb = this.snackBar.open("Do you want to install this App?", "Install", 
+            {duration: 5000});
+          sb.onAction().subscribe( () => {
+             (event as any).prompt();
+             (event as any).userChoice.then( result => {
+                if (result.outcome == "dismissed") {
+                  //TODO: Track no installation 
+                } else {
+                  //TODO: It was installed
+                }
+             });
+          });
+          return false;
+        });
+      }
   }
 }
