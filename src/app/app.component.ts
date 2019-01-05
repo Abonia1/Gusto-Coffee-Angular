c418cecf0db1b827647b1ae4d77e62deceab7e9d
Unknown <aboniaa@gmail.com>
2019-01-05 15:42:19 +0100

    ios "add as home app" message from  browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mycofeeapp';
+  constructor(private snackBar: MatSnackBar) {
+
+  }
+
+  ngOniit(){
+    if ((navigator as any).standalone==false) {
+      // This is an iOS device and we are in the browser
+      this.snackBar.open("You can add this PWA to the Home Screen", 
+          "", { duration: 3000 });
+    }
+    
+  }
 }
